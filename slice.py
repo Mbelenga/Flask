6220729d0690ff05930a42bf53667486c23c6c15
Mbelenga <mbelengaterence@gmail.com>
2024-08-18 15:00:58 +0300

    slicing a list

---
+++ slice.py
@@ -0,0 +1,11 @@
+"""
+Slicing a list in Python works by 
+specifying the index of the First and Last
+Element you want to work with
+"""
+players = ['Charles', 'Eli', 'Martina', 'June', 'Omondi']
+print(players[0:3])
+# Without a starting index python begins at the start of a list
+print(players[:4])
+# Without an ending index python goes to the end of a list
+print(players[2:])
