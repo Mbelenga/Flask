cb66b5f9af30a01302026813dfe19f646da19edd
Mbelenga <mbelengaterence@gmail.com>
2024-08-22 16:40:55 +0300

    Using if statements with lists

---
+++ toppings.py
@@ -0,0 +1,4 @@
+requested_toppings = ['Mushrooms', 'Green Peppers', 'Extra cheese']
+for requested_topping in requested_toppings:
+    print(f"Adding{requested_topping}.")
+print("\nFinished making your Pizza!")
