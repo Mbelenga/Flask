2dec743495f156b9c4d16f91b0bf6b64a2b42e06
Mbelenga <mbelengaterence@gmail.com>
2024-08-22 17:14:37 +0300

    Username

---
+++ If/Admin.py
@@ -0,0 +1,5 @@
+usernames = ['Admin', 'Pendo', 'Susan', 'Imani', 'Maria']
+for username in usernames:
+    print(f"Hello, {username} would you like to receive a status report!")
+else:
+    print(f"Thank you for logging in!")
