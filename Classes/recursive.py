df7b367e71cb546b6d8335682ceff71aaa1296a3
Mbelenga <mbelengaterence@gmail.com>
2024-10-31 02:48:29 +0300

    Updated

---
+++ Classes/recursive.py
@@ -0,0 +1,8 @@
+def reverse_string(s):
+    return s[::-1]
+    # reverse_string is a function
+    # s is the input string
+    # s[::-1] is the reversed string
+    # return s[::-1] is the output of the function
+print(reverse_string("Emobilis"))
+# output is silibomE
