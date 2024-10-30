7e640bac0715fac38fd717f8d89363f7efdbd339
Mbelenga <mbelengaterence@gmail.com>
2024-10-30 14:39:36 +0300

    creating a dog class

--- Classes/dog.py
+++ Classes/dog.py
@@ -14,5 +14,8 @@ class Dog:
         print(f"{self.name} rolled over!")
 
 my_dog = Dog("Fido", 6)
+my_dog.sit()
+my_dog.roll_over()
 print(f"My dog's name is {my_dog.name}")
 print(f"My dog is {my_dog.age} years old")
+m
