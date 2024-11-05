59dad535994ca96e1021313610443e248e7794cd
Mbelenga <mbelengaterence@gmail.com>
2024-11-05 23:39:15 +0300

    max and min

--- PYTHON/list.py
+++ PYTHON/list.py
@@ -1,4 +1,5 @@
 numbers = [12, 24, 50, 4, 30]
 
 largest_number = max(numbers)
-print(f"The largest number in the list is: {largest_number}")
+smallest_number =  min(numbers)
+print(f"The largest number in the list is: {largest_number} and the smallest number in the list is: {smallest_number}")
