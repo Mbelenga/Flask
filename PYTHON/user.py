9da88c1afaa35cfca8c47e1d1f8fd5c7fe7b8bdf
Mbelenga <mbelengaterence@gmail.com>
2024-11-05 22:31:02 +0300

    updated

---
+++ PYTHON/user.py
@@ -0,0 +1,5 @@
+user = "Okello"  # string
+age = 23 # integer
+height = 5.9  # float
+isDeveloper = True  # boolean
+print(f"My name is {user} and I am {age} years old, and my height is {height} feet")
