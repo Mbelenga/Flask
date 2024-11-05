0005bcdb578eb07b918c71c7d14ad9ff0c9f9e37
Mbelenga <mbelengaterence@gmail.com>
2024-11-05 22:45:37 +0300

    simple program

---
+++ PYTHON/input.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+operation = input("Enter your choice (1/2/3/4)")
