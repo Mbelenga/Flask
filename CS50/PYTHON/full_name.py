0204d4940f333be43c556ae6c5e3e67ef9bf89fe
Mbelenga <mbelengaterence@gmail.com>
2024-11-12 23:50:24 +0300

    updated

--- CS50/PYTHON/full_name.py
+++ CS50/PYTHON/full_name.py
@@ -1,5 +1,5 @@
-first_name = "Terrence"
-last_name = "Mbelenga"
+first_name = "Stella"
+last_name = "Mavuno"
 full_name = f"{first_name} {last_name}"
 message = f"Hello, {full_name.title()}!"
 print(message)
