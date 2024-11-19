f55d8e2f880c8a62a42aba6d05691b132d13c8d1
Mbelenga <mbelengaterence@gmail.com>
2024-11-19 23:46:31 +0300

    successfully created multiple instances

--- Classes/dog.py
+++ Classes/dog.py
@@ -20,3 +20,8 @@ print(f"My dog is {my_dog.age} years old")
 my_dog.sit()
 my_dog.roll_over()
 
+print(f"My dog's name is {your_dog.name}")
+print(f"My dog is {your_dog.age} years old")
+your_dog.sit()
+your_dog.roll_over()
+
