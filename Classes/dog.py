b20839c85c5d5be1c60d334c3d87beb2b5f78e10
Mbelenga <mbelengaterence@gmail.com>
2024-11-19 23:44:10 +0300

    Creating multiple instances

--- Classes/dog.py
+++ Classes/dog.py
@@ -14,7 +14,9 @@ class Dog:
         print(f"{self.name} rolled over!")
 
 my_dog = Dog("Fido", 6)
+your_dog = Dog('Chen', 3)
 print(f"My dog's name is {my_dog.name}")
 print(f"My dog is {my_dog.age} years old")
 my_dog.sit()
 my_dog.roll_over()
+
