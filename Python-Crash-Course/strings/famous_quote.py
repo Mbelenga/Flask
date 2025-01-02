17fdd78b4fa6e5866f69a4398d0863cd6f9d636e
Mbelenga <mbelengaterence@gmail.com>
2025-01-03 01:10:50 +0300

    updated

--- Python-Crash-Course/strings/famous_quote.py
+++ Python-Crash-Course/strings/famous_quote.py
@@ -1,2 +1,2 @@
-famous_quote = "'The only way to do great work is to love what you do.'"
-print(f"Steve Jobs once said, {famous_quote}")
+famous_quote = "The only way to do great work is to love what you do."
+print(f'Steve Jobs once said, "{famous_quote}" ')
