f507b4c5af1508e3963bacd7790a17c8722aa3e9
Mbelenga <mbelengaterence@gmail.com>
2025-01-03 00:52:35 +0300

    update the solution

--- Python-Crash-Course/strings/personal_message.py
+++ Python-Crash-Course/strings/personal_message.py
@@ -1,2 +1,2 @@
-personal_name = 'Eric'
-print(f' Hello, {personal_name} would you like to learn some python today')
+personal_name = "Eric"
+print(f"Hello {personal_name}, would you like to learn some Python today?")
