1c529b4fa6325febd9231e0cbb1240588a11dc40
Mbelenga <mbelengaterence@gmail.com>
2025-01-06 02:14:29 +0300

    updated

---
+++ Python-Crash-Course/Looping/interest.py
@@ -0,0 +1,16 @@
+# Have the user enter their investment amount and expected interest rate
+# Each year their investment will increase by their investment + their investment * interest rate
+# Print out their earnings after a 10 year period
+# Print out the earnings after 10 year period
+
+
+# Ask for the money invested and the interest rate
+
+
+# Convert the value to a float
+
+
+# Convert the value to a float and round the percentage rate by 2 digits
+
+
+# Cycle through 10 years using a for loop and range from 0 to 9
