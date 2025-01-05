226437da871adc8f6a5b42ecccb81ca1d344cd07
Mbelenga <mbelengaterence@gmail.com>
2025-01-06 01:57:09 +0300

    odds

---
+++ Python-Crash-Course/Looping/odds.py
@@ -0,0 +1,8 @@
+# Use for loop through the list from 1 to 21
+for i in range(1, 21):
+
+# Use modulus to check that the result is NOT EQUAL to 0
+    if ((i % 2) != 0):
+
+#print the odds
+        print("i = ", i)
