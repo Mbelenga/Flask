f181de08645250dc37eea9ac4e241c2d568462b8
Mbelenga <mbelengaterence@gmail.com>
2025-01-07 23:23:31 +0300

    still writing different programs with the alien color

--- Python-Crash-Course/Looping/aliens.py
+++ Python-Crash-Course/Looping/aliens.py
@@ -1,9 +1,9 @@
 alien_color = 'red'
 if alien_color == 'Green':
-    print('The player just earned 5 points')
+    print('The player just earned 5 points!')
 elif alien_color == 'Yellow':
-    print('The player just earned 10 points')
+    print('The player just earned 10 points!')
 elif alien_color == 'Red':
-    print('The player just earned 15 points')
+    print('The player just earned 15 points!')
 else:
-    print('Invalid alien color')
+    print('Invalid alien color!')
