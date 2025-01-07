d1c464300d57ca1578a70e23081159eb3505f220
Mbelenga <mbelengaterence@gmail.com>
2025-01-07 23:38:07 +0300

    A series of if statements that checks if a fruit is in my list

---
+++ Python-Crash-Course/Looping/fruits.py
@@ -0,0 +1,7 @@
+favorite_fruit = ['Apple', 'Banana', 'Cherry']
+if 'Apple' in favorite_fruit:
+    print("You really like Apples!")
+if 'Banana' in favorite_fruit:
+    print("You really like Bananas!")
+if 'Cherry' in favorite_fruit:
+    print("You really like Cherries!")
