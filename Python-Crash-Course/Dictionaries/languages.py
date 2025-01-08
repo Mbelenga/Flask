0c5d46a613a31ae2da155820efd2a46de4073b69
Mbelenga <mbelengaterence@gmail.com>
2025-01-09 01:33:18 +0300

    Learnt a dictionary of similar objects using this example

---
+++ Python-Crash-Course/Dictionaries/languages.py
@@ -0,0 +1,8 @@
+favorite_languages = {
+    'Wambui': 'Python',
+    'Kamau': 'Java',
+    'Kinyua': 'C++',
+    'Wanjiru': 'Python',
+    }
+language = favorite_languages['Wambui'].title()
+print(f"Wambui's favorite language is {language}.")
