681a160be45b6beaa9881d6ad8c479c12b007618
Mbelenga <mbelengaterence@gmail.com>
2025-01-11 23:18:34 +0300

    updated

--- Python-Crash-Course/Dictionaries/alien.py
+++ Python-Crash-Course/Dictionaries/alien.py
@@ -1,6 +1,6 @@
 alien_0 = {'color': 'green', 'points': 5}
-print(alien_0)
-
-alien_0['x_position'] = 0
-alien_0['y_position'] = 25
-print(alien_0)
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+1 aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+ print(alien)
