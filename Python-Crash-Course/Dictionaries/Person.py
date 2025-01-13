4f43e0aa71aa770e9bf62cb066f3595134388440
Mbelenga <mbelengaterence@gmail.com>
2025-01-14 02:29:32 +0300

    Writing a program using the dictionary concept

---
+++ Python-Crash-Course/Dictionaries/Person.py
@@ -0,0 +1,13 @@
+# Define a dictionary to store information about a person
+person = {
+    'first_name': 'Sharon',
+    'last_name': 'Mumbi',
+    'age': 21,
+    'city': 'Nairobi'
+}
+
+# Print each piece of information stored in the dictionary
+print(f"First Name: {person['first_name']}")
+print(f"Last Name: {person['last_name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
