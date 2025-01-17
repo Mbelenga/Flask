e0a134c3d4a855ac1df2e2ccad1dc8852051eba8
Mbelenga <mbelengaterence@gmail.com>
2025-01-17 15:03:12 +0300

    fixing the error

---
+++ Python-Crash-Course/Dictionaries/numbers.py
@@ -0,0 +1,11 @@
+# Dictionary to store people's favorite numbers
+favorite_numbers = {
+    'John', 21,
+    'Alice', 42,
+    'Bob', 45,
+    'Charlie', 67,
+    'David', 89
+}
+# Print each person's name and their favorite number
+for name, number in favorite_numbers.items():
+    print(f"{name}'s favorite number is {number}.")
