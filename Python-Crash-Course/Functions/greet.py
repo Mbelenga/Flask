4f0fb9eb57fb3d86c2767ad68f92867cba61c201
Mbelenga <mbelengaterence@gmail.com>
2025-01-17 15:33:49 +0300

    i have modified the code to call the name of the user

--- Python-Crash-Course/Functions/greet.py
+++ Python-Crash-Course/Functions/greet.py
@@ -1,4 +1,4 @@
-def greet_user():
-    """Displays a simple greeting."""
-    print("Hello User!")
-greet_user()
+def greet_user(username):
+    """ Displays the name of the user."""
+    print(f"Hello, {username.title()}!")
+greet_user('Maria')
