4a99a4c17077853d8c4df24d431a5ee05b1a16dd
Mbelenga <mbelengaterence@gmail.com>
2025-01-17 14:34:06 +0300

    updated

---
+++ Python-Crash-Course/Dictionaries/username.py
@@ -0,0 +1,9 @@
+user_0 = {
+    'username': 'Tajiri',
+    'first': 'Brian',
+    'last': 'Wambua',
+}
+
+for key, value in user_0.items():
+    print(f"\nKey: {Key}")
+    print(f"Value": {value}")
