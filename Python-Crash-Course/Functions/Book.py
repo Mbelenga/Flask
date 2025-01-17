280386aba06aec27c598a7eb2aa435252a6c0032
Mbelenga <mbelengaterence@gmail.com>
2025-01-17 16:28:19 +0300

    output is correct done

--- Python-Crash-Course/Functions/Book.py
+++ Python-Crash-Course/Functions/Book.py
@@ -5,5 +5,5 @@ favorite books is Alice in Wonderland. Call the function, making sure to
 include a book title as an argument in the function call.
 """
 def favorite_book(title):
-    print("One of my favorite books is {title}")
+    print(f"One of my favorite books is {title}")
 favorite_book("Alice in Wonderland")
