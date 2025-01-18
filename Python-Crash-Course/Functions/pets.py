dc9c81d8b431598c8f6a1a1cf614fcdf8a388907
Mbelenga <mbelengaterence@gmail.com>
2025-01-18 15:41:23 +0300

    multiple function calls

--- Python-Crash-Course/Functions/pets.py
+++ Python-Crash-Course/Functions/pets.py
@@ -1,6 +1,9 @@
 # The function tells us what kind of animal each pet is and the name of the pet.
 def describe_pet(animal_type, pet_name):
-    """Displays the name of the pet"""
+    """Displays information about the pet"""
     print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 describe_pet('dog', 'Rio')
+describe_pet('dog', 'Oki')
+describe_pet('dog', 'Snooby')
+describe_pet('cat', 'Grey')
