cb8dc2c88a94d31b103e347a2f001172de81cfea
Mbelenga <mbelengaterence@gmail.com>
2025-01-18 17:11:33 +0300

    a function that takes a first and last name, and returns a neatly formatted full name

--- Python-Crash-Course/Functions/musician.py
+++ Python-Crash-Course/Functions/musician.py
@@ -2,5 +2,5 @@ def get_formatted_name(first_name, last_name):
     """  This function returns a full name """
     full_name = f"{first_name} {last_name}"
     return full_name.title()
-musician = get_formatted_name('Alicia', 'Keys')
+musician = get_formatted_name('Selena', 'Gomez')
 print(musician)
