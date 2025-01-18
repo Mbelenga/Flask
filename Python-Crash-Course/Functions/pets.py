d1ceb9b44832b226ea53c2140d4fdb323e4b5a9f
Mbelenga <mbelengaterence@gmail.com>
2025-01-18 15:38:04 +0300

    A function that tells us what kind of an animal each pet is

--- Python-Crash-Course/Functions/pets.py
+++ Python-Crash-Course/Functions/pets.py
@@ -1,6 +1,6 @@
 # The function tells us what kind of animal each pet is and the name of the pet.
 def describe_pet(animal_type, pet_name):
     """Displays the name of the pet"""
-    print(f"\nThis is my {animal_type}.")
+    print(f"\nI have a {animal_type}.")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
-describe_pet('dog', 'willie')
+describe_pet('dog', 'Rio')
