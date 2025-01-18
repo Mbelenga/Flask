2e90e724d186505acef8ca65d5337b1b51951c14
Mbelenga <mbelengaterence@gmail.com>
2025-01-18 15:54:08 +0300

    A function called make_shirt() that accepts a size and the

--- Python-Crash-Course/Functions/make.py
+++ Python-Crash-Course/Functions/make.py
@@ -1,4 +1,4 @@
 def make_shirt(size, message):
     print(f"\nI will make a {size} t-shirt.")
     print(f"The message on the t-shirt will read: {message.title()}.")
-make_shirt('medium', 'Coding is fun!')
+make_shirt('Large', 'Coding is fun!')
