354705864ba985ab4f6b9b89d361e1baa2814135
Mbelenga <mbelengaterence@gmail.com>
2025-01-18 16:03:57 +0300

    A function that displays a city and its country

---
+++ Python-Crash-Course/Functions/cities.py
@@ -0,0 +1,4 @@
+def describe_city(city, country):
+    """ A function that displays a city and its country"""
+    print(f"\n{city.title()} is in {country.title()}.")
+describe_city('Nairobi', 'Kenya')
