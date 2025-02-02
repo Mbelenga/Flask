df1adfa84e1ade5bfb8691181989f263d37f0636
Mbelenga <138711355+Mbelenga@users.noreply.github.com>
2025-02-02 23:30:50 +0300

    Updated

--- Python-Crash-Course/classes/packages.py
+++ Python-Crash-Course/classes/packages.py
@@ -1,6 +1,6 @@
 class Package:
     def __init__(self, number, sender, recipient, weight):
-        self.number = number
+        self.number = numbe
         self.sender = sender
         self.recipient = recipient
         self.weight = weight
