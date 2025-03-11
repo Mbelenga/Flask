97fd5df6be4c5840f2aaf812f14c47665b4554eb
Mbelenga <mbelengaterence@gmail.com>
2025-03-11 03:54:41 +0300

    working on a small python challenge

---
+++ PYTHON/modulo.py
@@ -0,0 +1,9 @@
+"""
+Write a code that initializes three variables, a, b and c with the values 9, 2, and 11 (respectively).
+
+After that, initialize the following variables:
+
+d that will hold the result of a modulo 2 
+e that will hold the result of a modulo 3
+f that will hold the result of c modulo 10
+"""
