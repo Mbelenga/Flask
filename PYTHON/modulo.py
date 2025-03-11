e45984fc30c2bdf6867ac9668eea448b9f5d60d2
Mbelenga <mbelengaterence@gmail.com>
2025-03-11 03:58:36 +0300

    updated

--- PYTHON/modulo.py
+++ PYTHON/modulo.py
@@ -7,3 +7,9 @@ d that will hold the result of a modulo 2
 e that will hold the result of a modulo 3
 f that will hold the result of c modulo 10
 """
+a = 9
+b = 2
+c = 11
+d = a % 2
+e = a % 3
+f = c % 10
