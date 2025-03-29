a339a0503c0dda26c03e6c9844de903cc06640ba
Mbelenga <mbelengaterence@gmail.com>
2025-03-29 23:56:21 +0300

    creating and using a class in python

---
+++ Python-Crash-Course/classes/dog.py
@@ -0,0 +1,13 @@
+class Dog:
+    """A simple attempt to model a dog"""
+
+def __init__(self, name, age):
+    """Initializes name and age attributes"""
+    self.name = name
+    self.age = age
+
+def sit(self):
+    print(f"{self.name} is now sitting")
+
+def roll_over(self):
+    print(f"{self.name} is now rolling over")
