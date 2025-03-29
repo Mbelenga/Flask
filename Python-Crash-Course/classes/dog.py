d8bc5497a15e25246e5f7971f77eee4a85df71ac
Mbelenga <mbelengaterence@gmail.com>
2025-03-30 00:13:49 +0300

    fixing the error while trying again

--- Python-Crash-Course/classes/dog.py
+++ Python-Crash-Course/classes/dog.py
@@ -1,17 +1,5 @@
-class Dog:
-    """A simple attempt to model a dog"""
+The following Python class Dog is intended to model a simple dog with attributes name and age, and methods sit() and roll_over(). However, there are some errors in the code.
 
-def __init__(self, name, age):
-    """Initializes name and age attributes"""
-    self.name = name
-    self.age = age
+Identify the errors in the code.
 
-def sit(self):
-    print(f"{self.name} is now sitting")
-
-def roll_over(self):
-    print(f"{self.name} is now rolling over")
-
-my_dog = Dog('Rio', 7)
-print(f"My dog's name is {my_dog.name}")
-print(f"My dog's age is {my_dog.age}")
+Correct the code so that it runs properly.
