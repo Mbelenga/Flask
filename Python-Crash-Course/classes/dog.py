bf3ee36d5b72b2e8520df95c73852a0584329346
Mbelenga <mbelengaterence@gmail.com>
2025-03-30 00:25:00 +0300

    Corrected the error

--- Python-Crash-Course/classes/dog.py
+++ Python-Crash-Course/classes/dog.py
@@ -1,10 +1,3 @@
-"""
-The following Python class Dog is intended to model a simple dog with attributes name and age, and methods sit() and roll_over(). However, there are some errors in the code.
-
-Identify the errors in the code.
-
-Correct the code so that it runs properly.
-"""
 class Dog:
     def __init__(self, name, age):
         self.name = name
