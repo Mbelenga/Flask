297406f8c68b49767e380c84fd9299732fd8fa6c
Mbelenga <mbelengaterence@gmail.com>
2025-03-30 01:45:34 +0300

    Done

--- Python-Crash-Course/classes/dog.py
+++ Python-Crash-Course/classes/dog.py
@@ -1,19 +1,3 @@
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def sit(self):
-        print(f"{self.name} is now sitting")
-
-    def roll_over(self):
-        print(f"{self.name} is now rolling over")
-
-my_dog = Dog('Rio', 8)
-
-print(f"My dog's name is {my_dog.name}")
-print(f"My dog is {my_dog.age} years old")
-
 """
 Write a Python class called Dog that:
 
@@ -27,3 +11,17 @@ Create an instance of the Dog class with the name "Rio" and age 8.
 
 Print the dog's name and age using formatted strings.
 """
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name} is now sitting")
+    
+    def roll_over(self):
+        print(f"{self.roll_over}")
+
+my_dog = Dog("Rio", 8)
+print(f"My dog's name is {my_dog.name}")
+print(f"My dog is {my_dog.age} years old")
