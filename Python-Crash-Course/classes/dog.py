862186588cbaee7f0291d87407a360362d962d8f
Mbelenga <mbelengaterence@gmail.com>
2025-03-30 00:24:04 +0300

    updated

--- Python-Crash-Course/classes/dog.py
+++ Python-Crash-Course/classes/dog.py
@@ -1,5 +1,22 @@
+"""
 The following Python class Dog is intended to model a simple dog with attributes name and age, and methods sit() and roll_over(). However, there are some errors in the code.
 
 Identify the errors in the code.
 
 Correct the code so that it runs properly.
+"""
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name} is now sitting")
+
+    def roll_over(self):
+        print(f"{self.name} is now rolling over")
+
+my_dog = Dog('Rio', 8)
+
+print(f"My dog's name is {my_dog.name}")
+print(f"My dog is {my_dog.age} years old")
