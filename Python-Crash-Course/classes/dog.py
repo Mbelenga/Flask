2d1f7f3b1e7c4b8e2eedc9f2c5c47b3e84571f57
Mbelenga <mbelengaterence@gmail.com>
2025-03-30 00:10:51 +0300

    Fixing an error

--- Python-Crash-Course/classes/dog.py
+++ Python-Crash-Course/classes/dog.py
@@ -11,3 +11,7 @@ def sit(self):
 
 def roll_over(self):
     print(f"{self.name} is now rolling over")
+
+my_dog = Dog('Rio', 7)
+print(f"My dog's name is {my_dog.name}")
+print(f"My dog's age is {my_dog.age}")
