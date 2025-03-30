47c20f424068ff9eac898bfb46034f9feb1f611e
Mbelenga <mbelengaterence@gmail.com>
2025-03-30 19:05:12 +0300

    updated

--- Python-Crash-Course/classes/restaurant.py
+++ Python-Crash-Course/classes/restaurant.py
@@ -8,6 +8,14 @@ Make an instance called restaurant from your class. Print the two attributes ind
 and then call both methods.
 """
 class Restaurant:
-    def __init__(self, restaurant_name, cuisine_type)
-    self.
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name}")
+        print(f"{self.cuisine_type}")
+
     
+    def open_restaurant(self):
+        print('Restaurant Now Open')
