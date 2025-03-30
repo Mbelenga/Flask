294043ff0cd3a135169a4bd6cb80b2da86fa88e9
Mbelenga <mbelengaterence@gmail.com>
2025-03-30 18:37:04 +0300

    created multiple instances

--- Python-Crash-Course/classes/dog.py
+++ Python-Crash-Course/classes/dog.py
@@ -23,8 +23,11 @@ class Dog:
         print(f"{self.name} rolled over")
 
 my_dog = Dog("Rio", 8)
+your_dog = Dog("Oki", 3)
 print(f"My dog's name is {my_dog.name}")
 print(f"My dog is {my_dog.age} years old")
-
 my_dog.sit()
-my_dog.roll_over()
+
+print(f"\nMy dog's name is {your_dog.name}")
+print(f"My dog is {your_dog.age} years old")
+your_dog.sit()
