deaec253cda5da0832d227fb3867a2bcd2e17e5a
Mbelenga <mbelengaterence@gmail.com>
2025-04-01 00:47:56 +0300

    added another instance

--- Python-Crash-Course/classes/car.py
+++ Python-Crash-Course/classes/car.py
@@ -12,6 +12,11 @@ class Car:
         """descriptive name"""
         long_name = f"{self.make} {self.model} {self.year}"
         return long_name.title()
+    
+    def read_odometer(self):
+        """prints the odometer reading"""
+        print(f"This car has {self.odometer_reading} miles on it")
 
 my_new_car = Car('audi', 'a4', '2025')
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
