decb69fcb8cfefc9b9bacdce6cbb1bc24641e494
Mbelenga <mbelengaterence@gmail.com>
2025-05-05 21:42:41 +0300

    updated

--- Python-Crash-Course/classes/try.py
+++ Python-Crash-Course/classes/try.py
@@ -3,9 +3,9 @@ class Instructor:
         self.name = Instructor_name
         self.address = address
 
-    def display(self):
-        print(f"Hi, I am {self.name}")
+    def display(self, subject_name):
+        print(f"Hi, I am {self.name} am learning {subject_name}")
 
 Instructor_1 = Instructor("Mathew","Nairobi")
 print(Instructor_1.name)
-Instructor_1.display()
+Instructor_1.display("Python")
