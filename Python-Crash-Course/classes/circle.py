ba30ecc162be4032ff6b814a5d0e513d1419b8a2
Mbelenga <mbelengaterence@gmail.com>
2025-05-07 01:28:46 +0300

    Thos is an area of a circle

--- Python-Crash-Course/classes/circle.py
+++ Python-Crash-Course/classes/circle.py
@@ -1,2 +1,10 @@
 class Circle:
-    def __init__(self):
+    pi = 3.14
+    def __init__(self, radius=6):
+        self.radius = radius
+    
+    def get_circumference(self):
+        return 2 * self.pi * self.radius
+
+Circle_1 = Circle(4)
+print(Circle_1.get_circumference())
