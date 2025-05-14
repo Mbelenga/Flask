141595fac1b652a696934c3831a3807f2278b1b8
Mbelenga <mbelengaterence@gmail.com>
2025-05-14 20:34:58 +0300

    Updated

--- Python-Crash-Course/python-classes/oop_subclass.py
+++ Python-Crash-Course/python-classes/oop_subclass.py
@@ -10,3 +10,13 @@ class SchoolMember:
         print('(Name: "{}" Age: "{}")'.format(self.name, self.age), end=" ")
 
 class Teacher(SchoolMember):
+    """ Represents a Teacher """
+    def __init__(self, name, age, salary):
+        SchoolMember.__init__(self, name, age)
+        self.salary = salary
+        print("(Initialized Teacher: {}).format(self.name)")
+    
+    def tell(self):
+        SchoolMember.tell(self)
+        print('salary: "{:d}"'.format(self.salary))
+
