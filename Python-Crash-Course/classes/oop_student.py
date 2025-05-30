3d21e98d0f0e14ec8366571c9f7c12662098eb24
Mbelenga <mbelengaterence@gmail.com>
2025-05-31 00:01:04 +0300

    Practice

--- Python-Crash-Course/classes/oop_student.py
+++ Python-Crash-Course/classes/oop_student.py
@@ -1,19 +1,3 @@
-"""
-🚗 Python OOP Practice Question:
-Create a class called Student with the following:
-
-Attributes: name, age, and grade
-
-A method called display_info() that prints:
-
-"<name> is <age> years old and scored grade <grade>."
-
-Then:
-
-Create an object of the Student class with your own details.
-
-Call the display_info() method on that object.
-"""
 class Student:
     def __init__(self, name, age, grade):
         self.name = name
