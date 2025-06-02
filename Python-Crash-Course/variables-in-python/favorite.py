6c20eb080fb171b5125e4ea455eba63c4b92fb66
Mbelenga <mbelengaterence@gmail.com>
2025-06-02 03:12:01 +0300

    combining variables

---
+++ Python-Crash-Course/variables-in-python/favorite.py
@@ -0,0 +1,4 @@
+fav_movie = "Currently binge watching Orange is the new black and am loving it"
+fav_music = "My favorite music is mpishi by bien"
+
+favs = "Currently binge watching Orange is the new black and am loving it" + fav_movie + "My favorite music is mpishi by bien" + fav_music
