a4954a5ac43307abd3d115ca7da24f88336c9853
Mbelenga <mbelengaterence@gmail.com>
2025-06-03 00:15:02 +0300

    calculating the total bill

---
+++ Python-Crash-Course/variables-in-python/bill.py
@@ -0,0 +1,5 @@
+bill = float(input('How much is the bill? '))
+tax = float(input('What is the sales tax (percentage)? '))
+tip = float(input('How much of a tip (percentage)? '))
+
+tax_amount = (bill * tax / 100) #calculate the tax
