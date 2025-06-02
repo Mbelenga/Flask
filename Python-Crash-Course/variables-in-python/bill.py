78c2ef2f31d761e66f091b3536b9f03c801a16c4
Mbelenga <mbelengaterence@gmail.com>
2025-06-03 00:24:49 +0300

    Done

--- Python-Crash-Course/variables-in-python/bill.py
+++ Python-Crash-Course/variables-in-python/bill.py
@@ -2,4 +2,12 @@ bill = float(input('How much is the bill? '))
 tax = float(input('What is the sales tax (percentage)? '))
 tip = float(input('How much of a tip (percentage)? '))
 
-tax_amount = (bill * tax / 100) #calculate the tax
+tax_amount = (bill * tax) / 100  #calculate the tax
+total = bill + tax_amount
+
+tip_amount = (total * tip) / 100
+total = total + tip_amount
+
+total = round(total, 2)
+
+print("The total bill is$ ", total)
