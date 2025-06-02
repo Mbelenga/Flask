ee1c4094dd1e6e0cb8e22c02f9d501eb01ea986d
Mbelenga <mbelengaterence@gmail.com>
2025-06-02 23:36:13 +0300

    error

---
+++ Python-Crash-Course/variables-in-python/format.py
@@ -0,0 +1,4 @@
+fav_movie = input('My ')
+fav_singer = input('My all time favorite singer is Bien i also like Lil Maina')
+favs = "My favorite movie is {} and my all time favorite singer is {}".format(fav_movie, fav_singer)
+print(favs)
