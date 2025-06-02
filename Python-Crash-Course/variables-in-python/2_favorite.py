04c34060a6628c4736813f4eacb09dffc4dd7d57
Mbelenga <mbelengaterence@gmail.com>
2025-06-02 03:28:42 +0300

    Dupdated

--- Python-Crash-Course/variables-in-python/2_favorite.py
+++ Python-Crash-Course/variables-in-python/2_favorite.py
@@ -1,4 +1,5 @@
 fav_food = "My favorite food is Pilau and stew"
 fav_drink = "Favorite drink is an ice cold coca cola"
 
-favs = f {fav_food}. {fav_drink}
+favs = f "{fav_food}. {fav_drink}"
+print(favs)
