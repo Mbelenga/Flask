04f586c116a928ff52d51f9050d845b08db1ce7a
Mbelenga <mbelengaterence@gmail.com>
2025-06-04 17:13:30 +0300

    updated lists

--- Python-Crash-Course/Lists/list1.py
+++ Python-Crash-Course/Lists/list1.py
@@ -1,3 +1,8 @@
 list1 = ['Dog', 'cat', 'scoobydoo']
 list1.append('Hello')
 print(list1)
+list1.pop() # Removes the last item in the list
+print(list1)
+list1.pop(1) # Removes the 2nd item from the list
+print(list1)
+print('Dog' in list1) # checks if an item is in a list
