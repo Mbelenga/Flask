2a923d7a5555fb46cd4c768a22ad0b872c87f8b5
Mbelenga <mbelengaterence@gmail.com>
2025-06-06 16:09:06 +0300

    trying again

---
+++ Python-Crash-Course/Lists/planets.py
@@ -0,0 +1,11 @@
+planets = ['Sun', 'Mercury', 'Earth', 'Venus', 'Saturn']
+
+for planet in planets:
+    if (planet == 'sun'):
+        print(planet, "is not a planet")
+    
+    else:
+        print(planet, "is a planet")
+    
+    if (planet == 'Mercury'):
+        print(planet, 'is closest to the sun')
