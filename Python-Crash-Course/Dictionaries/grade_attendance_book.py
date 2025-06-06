bef0d7f81f26670521e7b2c64e3d2d69d5df2ff3
Mbelenga <mbelengaterence@gmail.com>
2025-06-07 01:26:28 +0300

    Sarah's attendance

--- Python-Crash-Course/Dictionaries/grade_attendance_book.py
+++ Python-Crash-Course/Dictionaries/grade_attendance_book.py
@@ -3,3 +3,9 @@ Billy = {
     'Grades': [80, 69, 75, 96, 82],
     'Attendance' : [True, True, True, True, True],
 }
+
+Sarah = {
+    'name': 'Sarah',
+    'Grades': [10, 72, 17, 79, 88],
+    'Attendance' : [True, False, True, False, True]
+}
