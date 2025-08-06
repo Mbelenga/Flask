8bd4c0ee7ae01b4a7987b9b06ce5e8b1ab9be06c
Mbelenga <mbelengaterence@gmail.com>
2025-08-07 01:00:08 +0300

    Inheritance

--- Python-Crash-Course/Oop_python.py
+++ Python-Crash-Course/Oop_python.py
@@ -9,3 +9,9 @@ class House(Building):
     def __init__(self, address, garden_size):
         super().__init__(address)
         self.garden_size = garden_size
+    
+    def describe(self):
+        return f"This house is at {self.address} and is also a building"
+    
+    def describe_print(self):
+        print(f"This house is blue and the garden is {self.garden_size} big")
