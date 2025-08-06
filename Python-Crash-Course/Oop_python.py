72c74f926245a61cf0be14ec0364663b222df7ff
Mbelenga <mbelengaterence@gmail.com>
2025-08-07 00:53:31 +0300

    Updated

--- Python-Crash-Course/Oop_python.py
+++ Python-Crash-Course/Oop_python.py
@@ -4,3 +4,8 @@ class Building:
     
     def describe(self):
         return f"This building is at {self.address}"
+
+class House(Building):
+    def __init__(self, address, garden_size):
+        super().__init__(address)
+        self.garden_size = garden_size
