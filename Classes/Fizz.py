68deeaf9874b62d93f9be04bf579c9953c041538
Mbelenga <mbelengaterence@gmail.com>
2026-01-20 22:40:13 +0300

    Updated

---
+++ Classes/Fizz.py
@@ -0,0 +1,19 @@
+"""
+// The Problem:
+    // Write a program that prints the number from 1 to 20
+    For multiples of three print "Fizz" instead of the number
+    For the multiples of five print "Buzz" instead of the number
+    For numbers which are multiples of both three and five print "FizzBuzz"
+
+// FOR LOOP:
+Set counter to 1
+Break when counter reaches 20
+Increment counter by 1
+    // IF NUMBER MOD 15 === 0
+        // print 'FizzBuzz'
+    // ELSE IF number MOD 3 === 0
+        // print 'Fizz'
+    // ELSE IF number MOD 5 === 0
+        // print 'Buzz'
+    // ELSE
+        // PRINT 'Fizz'
